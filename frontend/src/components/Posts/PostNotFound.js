9913c1175caffa140520257ed3b56fa0182fe3ac
Paulo <paulorcv@gmail.com>
2019-03-11 21:13:12 -0300

    postNotFound component and redirect

--- frontend/src/components/Posts/PostNotFound.js
+++ frontend/src/components/Posts/PostNotFound.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import Card from "../../components/Card/Card.jsx";
 import CardBody from "../../components/Card/CardBody.jsx";
 import postStyle from "../../assets/jss/material-kit-react/views/postStyle.jsx";
+import GridItem from "../../components/Grid/GridItem.jsx";
 
 
 
@@ -18,11 +18,13 @@ class PostNotFound extends Component {
       return (
         <div>
             <Grid container spacing={24} className={classes.gridContainer}>               
+            <GridItem xs={12} sm={12} lg={12} md={12} xl={12}>
               <Card>
               <CardBody>
                       <h2 className={classes.title} align='center'>POST NOT FOUND</h2>
               </CardBody>
               </Card>       
+              </GridItem>
             </Grid>     
         </div>
           )
@@ -49,5 +51,5 @@ function mapStateToProps( {posts}, props){
   }
 }
 
-export default connect(mapStateToProps)(withStyles(postStyle)(PostNotFound))
+export default withStyles(postStyle)(PostNotFound)
 
--- frontend/src/components/Posts/PostPage.js
+++ frontend/src/components/Posts/PostPage.js
@@ -6,7 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import PostEdit from './PostEdit';
 import Post from './Post';
 import PostNew from './PostNew';
-import { Redirect } from 'react-router-dom'
+import PostNotFound from './PostNotFound';
 
 
 const styles = theme => ({
@@ -29,30 +29,35 @@ class PostPage extends Component {
   render() {
     const { classes, category, id, post , action}  = this.props;
     
-     if(post.deleted === true || post.id===undefined ){
-      return <Redirect to={`/${category}/post/notfound`} />  
-     } 
-      
-
-
       return (
-        <div>
-            <Grid container spacing={24} className={classes.gridContainer}>
-                  <Grid key={id} item xs={12} sm={12} lg={12} xl={12}>                    
-                    { action === "edit" &&  (
-                      <PostEdit category={category}/>
-                    )}              
-                    { action === "view" &&  (
-                      <Post post={post} category={category}/>
-                    )}              
-                    { action === "new" &&  (
-                      <PostNew  category={category} />
-                    )}              
 
-              </Grid>
-            </Grid>     
-        </div>
-          )
+        (post.deleted === true || post.id===undefined ) ? 
+        (
+          <PostNotFound />
+        )  : 
+        (   
+          <div>
+          <Grid container spacing={24} className={classes.gridContainer}>
+          
+                <Grid key={id} item xs={12} sm={12} lg={12} xl={12}>                    
+                  { action === "edit" &&  (
+                    <PostEdit category={category}/>
+                  )}              
+                  { action === "view" &&  (
+                    <Post post={post} category={category}/>
+                  )}              
+                  { action === "new" &&  (
+                    <PostNew  category={category} />
+                  )}              
+
+            </Grid>
+          </Grid>     
+      </div> 
+      )
+          
+                  
+       
+      )
   }
 }
 
